5f59438026f9388d56a6c18bd815aa78c19a6a03
Freddie John Millman <freddie.millman@icloud.com>
2019-02-03 16:35:57 +0000

    Add styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,50 @@
-function IndexPage() {
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+            width: 1100,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    title: {
+        flex: 1,
+    },
+    markdown: {
+        padding: `${theme.spacing.unit * 3}px 0`,
+    }
+});
+
+function IndexPage(props) {
+    const { classes } = props;
+
   return (
-    <div>
-      Hello World!
-    </div>
+      <React.Fragment>
+        <div className={classes.layout}>
+            <Typography
+                component={"h2"}
+                variant={"h5"}
+                color={"inherit"}
+                align={"center"}
+                noWrap
+                className={classes.title}
+            >
+                Hello World!
+            </Typography>
+        </div>
+      </React.Fragment>
   );
 }
 
-export default IndexPage;
+IndexPage.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(IndexPage);
