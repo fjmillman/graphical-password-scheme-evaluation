2fafc14f03f6c4e6384324bea6d45345ed839c3e
Freddie John Millman <freddie.millman@icloud.com>
2019-02-04 18:06:34 +0000

    Exchange react-recollect for react-easy-state and implement some state

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { store, view } from 'react-easy-state'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { increment } from './store/updaters/increment'
 
 const styles = theme => ({
     layout: {
         width: 'auto',
-        marginTop: theme.spacing.unit * 3,
         marginLeft: theme.spacing.unit * 3,
         marginRight: theme.spacing.unit * 3,
         [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
@@ -14,24 +17,59 @@ const styles = theme => ({
             marginLeft: 'auto',
             marginRight: 'auto',
         },
-    }
+    },
+    toolbarMain: {
+        borderBottom: `1px solid ${theme.palette.grey[300]}`,
+    },
+    toolbarTitle: {
+        flex: 1,
+    },
+    text: {
+        margin: theme.spacing.unit * 3,
+    },
+    button: {
+        margin: theme.spacing.unit,
+    },
 });
 
 function IndexPage(props) {
     const { classes } = props;
 
+    const observations = store({
+        stage: 0,
+        content: (
+            <Typography component={"p"} align={"center"} className={classes.text}>
+                Welcome to the study!kjsbldfjkbasd
+            </Typography>
+        )
+    });
+
     return (
         <React.Fragment>
             <div className={classes.layout}>
-                <Typography
-                    component={"h2"}
-                    variant={"h5"}
-                    color={"inherit"}
-                    align={"center"}
-                    noWrap
+                <Toolbar className={classes.toolbarMain}>
+                    <Typography
+                        component={"h2"}
+                        variant={"h5"}
+                        color={"inherit"}
+                        align={"center"}
+                        noWrap
+                        className={classes.toolbarTitle}
+                    >
+                        Graphical Password Scheme User Study
+                    </Typography>
+                </Toolbar>
+                <main>
+                    {observations.content}
+                </main>
+                <Button
+                    variant="contained"
+                    href="#contained-buttons"
+                    className={classes.button}
+                    onClick={increment}
                 >
-                    Hello World!
-                </Typography>
+                    <Typography variant="subtitle1" color="primary">Continue</Typography>
+                </Button>
             </div>
         </React.Fragment>
     );
@@ -41,4 +79,4 @@ IndexPage.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(IndexPage);
+export default view(withStyles(styles)(IndexPage));
