74f268db42bdce9cde23be2c74f5c3fcee76e6ec
Freddie John Millman <freddie.millman@icloud.com>
2019-02-04 18:03:36 +0000

    Factor out Header into a component

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,27 @@
 import React from "react";
 import App, { Container } from "next/app";
-import Head from "next/head";
+import Header from "./components/Header";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 export default class EvaluationApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+        if (Component.getInitialProps) {
+          pageProps = await Component.getInitialProps(ctx);
+        }
+
+        return { pageProps };
     }
 
-    return { pageProps };
-  }
-
-  renderHead() {
-    return (
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-        />
-      </Head>
-    );
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <Container>
-        {this.renderHead()}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </Container>
-    );
-  }
+    render() {
+        const { Component, pageProps } = this.props;
+        return (
+            <Container>
+                <Header />
+                <CssBaseline />
+                <Component {...pageProps} />
+            </Container>
+        );
+    }
 }
---
+++ pages/components/Header.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+const Header = () => (
+    <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+    </Head>
+);
+
+export default Header
