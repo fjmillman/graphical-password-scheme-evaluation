35f18fe92bb81d7bc343886377eda0c12d885fa8
Freddie John Millman <freddie.millman@icloud.com>
2019-02-28 13:17:48 +0000

    Remove duplicate forward slash

--- www/store/updaters/submit.js
+++ www/store/updaters/submit.js
@@ -5,7 +5,7 @@ import { nextPage } from "./nextPage";
 
 export async function submit() {
     const baseUrl = url.format({  protocol: "https", host: window.location.hostname, pathname: "/" });
-    const apiUrl = process.env.NODE_ENV === "production" ? `${baseUrl}/api/result` : "http://localhost:9999/api/result";
+    const apiUrl = process.env.NODE_ENV === "production" ? `${baseUrl}api/result` : "http://localhost:9999/api/result";
 
     await axios.post(apiUrl, state.result || {})
         .then(() => console.log("Results uploaded to database"))
