1e642c33a6be180a35eed1dc0ea1f6cb3343efcb
Freddie John Millman <freddie.millman@icloud.com>
2019-02-28 13:29:01 +0000

    Deal with post url

--- www/store/updaters/submit.js
+++ www/store/updaters/submit.js
@@ -4,7 +4,13 @@ import state from "../state";
 import { nextPage } from "./nextPage";
 
 export async function submit() {
-    const baseUrl = url.format({  protocol: "https", host: window.location.hostname, pathname: "/" });
+    let protocol = "https";
+    let host = window.location.hostname;
+    if (host.indexOf("localhost") > -1) {
+        host = "localhost:3000";
+        protocol = "http";
+    }
+    const baseUrl = url.format({  protocol, host, pathname: "/" });
     const apiUrl = process.env.NODE_ENV === "production" ? `${baseUrl}api/result` : "http://localhost:9999/api/result";
 
     await axios.post(apiUrl, state.result || {})
