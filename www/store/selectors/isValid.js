8df745f41d9186fc8d11241fefcaec494056868e
Freddie John Millman <freddie.millman@icloud.com>
2019-03-04 22:04:15 +0000

    Fix login logic

--- www/store/selectors/isValid.js
+++ www/store/selectors/isValid.js
@@ -2,12 +2,12 @@ import state from "../state";
 
 export function isValid() {
     if (state.icons.flag.some(flag => state.passIcons.includes(flag))) {
-        return state.selected.forEach(icon => state.icons.pass.includes(icon)).filter(Boolean).length >= 1;
+        return state.selected.filter(icon => state.icons.pass.includes(icon)).length >= 1;
     }
 
     if (state.icons.skip.some(skip => state.passIcons.includes(skip))) {
-        return state.selected.forEach(icon => state.icons.skip.includes(icon)).filter(Boolean).length === 1 &&
-            state.selected.forEach(icon => state.icons.pass.includes(icon)).filter(Boolean).length === 5;
+        return state.selected.filter(icon => state.icons.skip.includes(icon)).length === 1 &&
+            state.selected.filter(icon => state.icons.pass.includes(icon)).length === 5;
     }
 
     return true;
