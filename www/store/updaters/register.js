c7fc13dba4a448bf5b2d70f964161745f5fa6ae3
Freddie John Millman <freddie.millman@icloud.com>
2019-03-07 23:39:55 +0000

    Block registration if icons not selected

--- www/store/updaters/register.js
+++ www/store/updaters/register.js
@@ -4,6 +4,8 @@ import { endTimer } from "./endTimer";
 import { startTimer } from "./startTimer";
 
 export function register() {
+    if (state.icons.pass.length < 6 || state.icons.skip.length < 3 || state.icons.skip.length < 3) return;
+
     state.icons.time = endTimer();
     state.authentication.registration = state.icons;
     state.currentPassIcons = getRandomStage();
